3b6666ff1a9e8d9917ee3171728dcc0a65f2c95c
ci <seksant98@gmail.com>
2016-11-26 16:27:18 +0700

    เพิ่ม bootstrep font

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,7 +10,14 @@ class Home extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('layout/header_view');
         $this->load->view('home_views');
+        $this->load->view('layout/footer_views');
+    }
+    public function test()
+    {
+        $this->load->view('layout/header_view');
+        $this->load->view('test_view');
     }
 
 }
---
+++ application/views/test_view.php
@@ -0,0 +1,11 @@
+<
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut commodi consectetur dolor doloribus eius et fugit id,
+    iure laudantium minima placeat quasi qui quidem recusandae sapiente sed tempora voluptatibus voluptatum!
+</div>
+<div>Alias consequatur delectus earum eius fuga obcaecati quaerat repellat repudiandae sint voluptas? A, alias,
+    aspernatur atque dicta id ipsam laboriosam nemo quas quibusdam, quis sint sunt veniam vero. Earum, molestiae?
+</div>
+<div>Amet dolorum eligendi eveniet nostrum officia officiis praesentium quam repellat soluta veritatis. A aliquid amet
+    atque consectetur deleniti distinctio dolores esse facere illo molestias, nesciunt nostrum optio perferendis quidem
+    saepe?
+</div>>
